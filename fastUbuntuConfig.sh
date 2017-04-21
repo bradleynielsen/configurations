a979273cfe671e4f9dc757637c6653c69861c0b5
Brad <bradleynielsen@gmail.com>
2017-04-21 12:52:39 -0500

    added install lines mongo yarn

--- fastUbuntuConfig.sh
+++ fastUbuntuConfig.sh
@@ -4,7 +4,7 @@ echo ''
 echo '          _    _  _                    _          '
 echo '         | |  | || |                  | |         '
 echo '         | |  | || |__   _   _  _ __  | |_  _   _ '
-echo '         | |  | || '_ \ | | | || '_ \ | __|| | | |'
+echo '         | |  | || |_ \ | | | || |_ \ | __|| | | |'
 echo '         | |__| || |_) || |_| || | | || |_ | |_| |'
 echo '          \____/ |_.__/  \__,_||_| |_| \__| \__,_|'
 echo '                                                  '
@@ -12,7 +12,7 @@ echo '                                                  '
 echo '           _____                __  _                            _               '
 echo '          / ____|              / _|(_)                          | |              '
 echo '         | |      ___   _ __  | |_  _   __ _  _   _  _ __  __ _ | |_  ___   _ __ '
-echo '         | |     / _ \ | '_ \ |  _|| | / _` || | | || '__|/ _` || __|/ _ \ | '_d_|'
+echo '         | |     / _ \ | \_ \ |  _|| | / _  || | | ||  __|/ _ ` || __|/ _ \ | __|'
 echo '         | |____| (_) || | | || |  | || (_| || |_| || |  | (_| || |_| (_) || |   '
 echo '          \_____|\___/ |_| |_||_|  |_| \__, | \__,_||_|   \__,_| \__|\___/ |_|   '
 echo '                                        __/ |                                    '
@@ -21,7 +21,7 @@ echo ''
 
 
 #Enable the script by:
-#:~$ " chmod +x ubuntuconfig.sh "
+#:~$ " chmod +x fastUbuntuConfig.sh "
 
 #####################
 #Update Repositories
@@ -44,6 +44,14 @@ sudo add-apt-repository -y ppa:webupd8team/atom  -y >> install.log
 
 echo "Adding the Variety repository..."
 sudo add-apt-repository  ppa:peterlevi/ppa -y >> install.log
+
+echo "Adding the Mongodb repository..."
+sudo apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv 0C49F3730359A14518585931BC711F9BA15703C6
+#ubuntu 14.04
+echo "deb [ arch=amd64 ] http://repo.mongodb.org/apt/ubuntu trusty/mongodb-org/3.4 multiverse" | sudo tee /etc/apt/sources.list.d/mongodb-org-3.4.list
+# #ubuntu 16.04
+# echo "deb [ arch=amd64,arm64 ] http://repo.mongodb.org/apt/ubuntu xenial/mongodb-org/3.4 multiverse" | sudo tee /etc/apt/sources.list.d/mongodb-org-3.4.list
+
 
 
 ########
@@ -86,7 +94,7 @@ echo "Installing Unity Tweak Tool"
 sudo apt-get install  unity-tweak-tool -y >> install.log
 
 #Atom
-echo "Installing Variety..."
+echo "Installing Atom..."
 sudo apt-get install atom -y >> install.log
 
 #Variety desktop images
@@ -95,6 +103,9 @@ sudo apt-get install variety variety-slideshow -y >> install.log
 
 echo "Installing httpie..."
 sudo apt-get install httpie -y >> install.log
+
+echo "Installing mongo..."
+sudo apt-get install mongodb-org -y >> install.log
 
 
 ###############
@@ -110,12 +121,17 @@ curl -sL https://deb.nodesource.com/setup_6.x | sudo -E bash -
 sudo apt-get install -qy nodejs >> out.log 2 >> err.log
 sudo chown -R $USER /usr/lib/node_modules/
 
+
 #mongo-hacker
 sudo npm i -gy mongo-hacker --loglevel silent
 #surge webpack budo
 sudo npm i -gy react-native-cli --loglevel silent
 #nodemon
 sudo npm i -gy nodemon --loglevel silent
+#yarn
+sudo npm i -gy yarn --loglevel silent
+
+
 
 ######################
 #Configure OS Settings
