3948a02b01e4b14764f9b1a86c02f71fe4271a46
Brad <bradleynielsen@gmail.com>
2017-05-16 02:09:53 -0500

    addedchoco

---
+++ WindowsChocoInstScrpt.cmd
@@ -0,0 +1,25 @@
+choco install windirstat  -y
+choco install chocolatey-core.extension  -y
+choco install curl  -y
+choco install gimp  -y
+choco install sublimetext3.packagecontrol  -y
+choco install audacity  -y
+choco install googledrive  -y
+choco install qbittorrent  -y
+choco install yarn -y
+choco install slack -y
+choco install cccp -y
+choco install openoffice -y
+rem choco install nvidia-display-driver -y
+rem choco install spotify  -y
+rem choco install visualstudio-github -y
+rem choco install microsoft-build-tools  -y
+rem choco install googlechrome-allusers -y
+rem choco install sublimetext3  -y
+rem choco install atom   -y
+rem choco install mongodb -y
+rem choco install 7zip  -y
+rem choco install nodejs -y
+rem choco install cmder  -y
+rem choco install git -y
+rem choco install putty -y
