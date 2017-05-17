3daf36911c70657485565397683f8c26b57422af
Brad Nielsen <bradleynielsen@gmail.com>
2017-05-17 03:16:08 -0700

    Update WindowsChocoInstScrpt.cmd

--- WindowsChocoInstScrpt.cmd
+++ WindowsChocoInstScrpt.cmd
@@ -1,10 +1,11 @@
 @powershell -NoProfile -ExecutionPolicy Bypass -Command "iex ((New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/install.ps1'))" && SET "PATH=%PATH%;%ALLUSERSPROFILE%\chocolatey\bin"
 choco upgrade chocolatey
-choco install googlechrome   -y
+choco install googlechrome-allusers -y
 choco install windirstat  -y
 choco install chocolatey-core.extension  -y
 choco install curl  -y
 choco install gimp  -y
+choco install sublimetext3  -y
 choco install sublimetext3.packagecontrol  -y
 choco install audacity  -y
 choco install googledrive  -y
@@ -13,10 +14,6 @@ choco install yarn -y
 choco install slack -y
 choco install cccp -y
 choco install openoffice -y
-choco install nvidia-display-driver -y
-choco install microsoft-build-tools  -y
-choco install googlechrome-allusers -y
-choco install sublimetext3  -y
 choco install atom   -y
 choco install mongodb -y
 choco install 7zip  -y
