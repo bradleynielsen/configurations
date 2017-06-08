5ffe35bbad953509980778b29724e52adae655a2
Brad Nielsen <bradleynielsen@gmail.com>
2017-06-08 01:22:14 -0500

    Update spotify.sh

--- spotify.sh
+++ spotify.sh
@@ -1,7 +1,7 @@
 #spotify
 echo "Spotify install init..."
 echo "Adding key..."
-apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv-keys 
+apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv-keys BBEBDCB318AD50EC6865090613B00F1FD2C19886
 echo deb http://repository.spotify.com stable non-free | sudo tee /etc/apt/sources.list.d/spotify.list
 echo "Running update..."
 sudo apt-get -qq -qq update
